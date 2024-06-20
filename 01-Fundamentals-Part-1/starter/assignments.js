9807fab246a7927503d91af7f2d4880ca8b13eba
Randall <randallmarcu@gmail.com>
2024-06-20 16:01:22 -0600

    Sección 2 completada

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+// Values and variables
+let country = "Mexico";
+let continent = "America";
+let population = "129331625";
+
+console.log(country);
+console.log(continent);
+console.log(population);
